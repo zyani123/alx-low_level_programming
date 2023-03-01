1aacbf66e34cd5a27de1c11923af9f513a0138b0
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-03-01 20:57:33 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,7 +5,6 @@
  * @n: integer params
  * Return: 0
  */
-
 void rev_string(char *n)
 {
 	int i = 0;
@@ -70,7 +69,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j--;
 		i--;
 	}
-if (digits == size_r)
+	if (digits == size_r)
 		return (0);
 	*(r + digits) = '\0';
 	rev_string(r);
