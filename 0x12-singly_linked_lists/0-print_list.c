21aac66baa18b062874dd9818cdf9fe82bf84982
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-04-27 21:33:13 +0000

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,5 @@
 #include "lists.h"
 
-/**
- * print_list - prints all the elements of a list_t list.
- * @h: pointer to the list.
- * Return: number of nodes.
- **/
 size_t print_list(const list_t *h)
 {
 	size_t cont = 0;
