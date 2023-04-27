87551915f08cd5875d08e91972ac12258033965d
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-04-27 21:42:08 +0000

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,10 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%u] %s\n", h->len, h->str);
-		else
 			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+			
 		cont++;
 		h = h->next;
 	}
