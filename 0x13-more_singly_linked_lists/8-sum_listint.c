8a587dd649baec0b28eb76a095481277dd70202d
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-05-03 02:29:32 +0100

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,5 @@
 #include "lists.h"
-/**
- * sum_listint - Write a function that returns the sum of all
- * the data (n) of a listint_t linked list.
- * @head: linked list's begining.
- * Return: the adding of the list values.
- */
+
 int sum_listint(listint_t *head)
 {
 	size_t i = 0;
