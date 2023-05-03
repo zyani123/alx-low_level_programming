9b8e5b2567c7d67d3cbd97adfec5d2f986431504
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-05-03 02:29:55 +0100

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,10 +1,5 @@
 #include "lists.h"
-/**
- * pop_listint - Write a function that deletes the head node of a listint_t
- * linked list, and returns the head node’s data (n).
- * @head: begin of linked list.
- * Return: Head node's data.
-*/
+
 int pop_listint(listint_t **head)
 {
 	listint_t *p;
