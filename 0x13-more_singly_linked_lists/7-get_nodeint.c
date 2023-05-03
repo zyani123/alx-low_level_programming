e04ed16b7b2eae210c0410fd6395271efdc062a3
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-05-03 02:30:08 +0100

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,5 @@
 #include "lists.h"
-/**
- * get_nodeint_at_index - Write a function that returns the nth
- * node of a listint_t linked list.
- * @index: node to look for.
- * @head: list's begining.
- * Return: the length of the list.
- */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
