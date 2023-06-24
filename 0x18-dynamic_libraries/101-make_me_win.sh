42e64e0137d6e860ff6cd12ef6e74af168b4d405
Your Name <you@example.com>
2023-06-23 17:37:55 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp https://github.com/zyani123/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp https://github.com/zyani123/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
